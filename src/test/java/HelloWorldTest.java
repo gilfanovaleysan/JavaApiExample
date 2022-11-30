6aa4761975e651c99c85a6786a46e4c366eaab25
Leysan <l.gilfanova.kazanexpress@gmail.com>
2022-11-30 19:08:22 +0300

    1.3. homework

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,9 +1,18 @@
 import org.junit.jupiter.api.Test;
-
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 public class HelloWorldTest {
 
     @Test
-    public void testHelloWorld(){
-        System.out.println("Hello World!");
+    public void testHelloWorldLeysan(){
+        System.out.println("Hello from Leysan");
+    }
+
+    @Test
+    public void firstApiTest(){
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/hello")
+                .andReturn();
+        response.prettyPrint();
     }
 }
