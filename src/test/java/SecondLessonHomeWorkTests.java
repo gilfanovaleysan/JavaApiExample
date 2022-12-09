fd43378d3255fea579c59404c79ba1357f040f0b
Leysan <l.gilfanova.kazanexpress@gmail.com>
2022-12-09 12:49:54 +0700

    first homework of second week

---
+++ src/test/java/SecondLessonHomeWorkTests.java
@@ -0,0 +1,21 @@
+import io.restassured.RestAssured;
+import io.restassured.http.Headers;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SecondLessonHomeWorkTests {
+
+    @Test
+    public void firstHomeworkTest() {
+        JsonPath response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_json_homework")
+                .jsonPath();
+
+        String answer = response.get("messages[1].message");
+        System.out.println(answer);
+    }
+}
