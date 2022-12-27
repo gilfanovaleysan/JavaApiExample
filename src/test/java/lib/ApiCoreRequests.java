3589e00ca1d57a904583d25e70093307b3bc7b07
Leysan <l.gilfanova.kazanexpress@gmail.com>
2022-12-27 10:39:30 +0300

    ex15

--- src/test/java/lib/ApiCoreRequests.java
+++ src/test/java/lib/ApiCoreRequests.java
@@ -4,6 +4,7 @@ import io.qameta.allure.Step;
 import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.http.Header;
 import io.restassured.response.Response;
+import org.apache.commons.lang3.RandomStringUtils;
 
 import java.util.Map;
 
@@ -43,6 +44,48 @@ public class ApiCoreRequests {
         return given()
                 .filter(new AllureRestAssured())
                 .body(authData)
+                .post(url)
+                .andReturn();
+    }
+
+    @Step("Make a POST-request with incorrect email")
+    public Response makeRegisterPostRequestWithIncorrectEmail(String url, Map<String, String> userData) {
+        String email = "vinkotovexample.com";
+        userData.put("email", email);
+        return given()
+                .filter(new AllureRestAssured())
+                .body(userData)
+                .post(url)
+                .andReturn();
+    }
+
+    @Step("Make a POST-request with short firstname")
+    public Response makeRegisterPostRequestWithShortFirstname(String url, Map<String, String> userData) {
+        String firstName = "X";
+        userData.put("firstName", firstName);
+        return given()
+                .filter(new AllureRestAssured())
+                .body(userData)
+                .post(url)
+                .andReturn();
+    }
+
+    @Step("Make a POST-request with long firstname")
+    public Response makeRegisterPostRequestWithLongFirstname(String url, Map<String, String> userData) {
+        String firstName = RandomStringUtils.randomAlphabetic(255, 300);
+        userData.put("firstName", firstName);
+        return given()
+                .filter(new AllureRestAssured())
+                .body(userData)
+                .post(url)
+                .andReturn();
+    }
+
+    @Step("Make a POST-request with empty field")
+    public Response makeRegisterPostRequestWithEmptyField(String url, Map<String, String> userData) {
+        return given()
+                .filter(new AllureRestAssured())
+                .body(userData)
                 .post(url)
                 .andReturn();
     }
--- src/test/java/tests/UserRegisterTest.java
+++ src/test/java/tests/UserRegisterTest.java
@@ -2,15 +2,23 @@ package tests;
 
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import lib.ApiCoreRequests;
 import lib.Assertions;
 import lib.BaseTestCase;
 import lib.DataGenerator;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Stream;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 public class UserRegisterTest extends BaseTestCase {
+
+    private final ApiCoreRequests apiCoreRequests = new ApiCoreRequests();
 
     @Test
     public void testCreateUserWithExistingEmail() {
@@ -42,5 +50,65 @@ public class UserRegisterTest extends BaseTestCase {
 
         Assertions.assertResponseCodeEquals(responseCreateAuth, 200);
         Assertions.assertJsonHasField(responseCreateAuth, "id");
+    }
+
+    @Test
+    public void testCreateUserWithIncorrectEmail() {
+        Map<String, String> userData = new HashMap<>();
+        userData = DataGenerator.getRegistrationData(userData);
+
+        Response responseCreateAuth = apiCoreRequests.makeRegisterPostRequestWithIncorrectEmail("https://playground.learnqa.ru/api/user/", userData);
+
+        Assertions.assertResponseCodeEquals(responseCreateAuth, 400);
+        Assertions.assertResponseTextEquals(responseCreateAuth, "Invalid email format");
+    }
+
+    @Test
+    public void testCreateUserWithTooShortFirstname() {
+        Map<String, String> userData = new HashMap<>();
+        userData = DataGenerator.getRegistrationData(userData);
+
+        Response responseCreateAuth = apiCoreRequests.makeRegisterPostRequestWithShortFirstname("https://playground.learnqa.ru/api/user/", userData);
+
+        Assertions.assertResponseCodeEquals(responseCreateAuth, 400);
+        Assertions.assertResponseTextEquals(responseCreateAuth, "The value of 'firstName' field is too short");
+    }
+
+    @Test
+    public void testCreateUserWithTooLongFirstname() {
+        Map<String, String> userData = new HashMap<>();
+        userData = DataGenerator.getRegistrationData(userData);
+
+        Response responseCreateAuth = apiCoreRequests.makeRegisterPostRequestWithLongFirstname("https://playground.learnqa.ru/api/user/", userData);
+
+        Assertions.assertResponseCodeEquals(responseCreateAuth, 400);
+        Assertions.assertResponseTextEquals(responseCreateAuth, "The value of 'firstName' field is too long");
+    }
+
+    @MethodSource("userData")
+    @ParameterizedTest(name = " {0}")
+    @DisplayName("Создание пользователя без указания поля")
+    public void testCreateUserWithEmptyField(String type, String email, String password, String username, String firstName, String lastName) {
+        Map<String, String> userData = new HashMap<>();
+        userData.put("email", email);
+        userData.put("password", password);
+        userData.put("username", username);
+        userData.put("firstName", firstName);
+        userData.put("lastName", lastName);
+
+        Response responseCreateAuth = apiCoreRequests.makeRegisterPostRequestWithEmptyField("https://playground.learnqa.ru/api/user/", userData);
+
+        Assertions.assertResponseCodeEquals(responseCreateAuth, 400);
+        Assertions.assertResponseTextEquals(responseCreateAuth, "The value of " + type + " field is too short");
+    }
+
+    static Stream<Arguments> userData() {
+        return Stream.of(
+                arguments("'email'", "", "123", "learnqa", "learnqa", "learnqa"),
+                arguments("'password'", DataGenerator.getRandomEmail(), "", "learnqa", "learnqa", "learnqa"),
+                arguments("'username'", DataGenerator.getRandomEmail(), "123", "", "learnqa", "learnqa"),
+                arguments("'firstName'", DataGenerator.getRandomEmail(), "123", "learnqa", "", "learnqa"),
+                arguments("'lastName'", DataGenerator.getRandomEmail(), "123", "learnqa", "learnqa", "")
+        );
     }
 }
